fcfbf946bdf6733143487c1a4802538738c4bd39
rubywangruobin <rubywang1029@gmail.com>
2020-12-17 18:58:00 -0600

    Update index.blade.php

--- resources/views/frontEnd/events/index.blade.php
+++ resources/views/frontEnd/events/index.blade.php
@@ -2,7 +2,8 @@
 @section('title')
 Events
 @stop
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+<link href="{{ URL::asset('css/main.css') }}" rel='stylesheet' />
+<script src="{{ URL::asset('js/main.js') }}"></script>
 <style>
     .button {
         border: none;
@@ -12,17 +13,21 @@ Events
         display: inline-block;
         font-size: 16px;
         }
+    #calendar {
+    max-width: 1100px;
+    margin: 0 auto;
+  }
 </style>
 @section('content')
 @include('layouts.filter_event')
 @include('layouts.sidebar_organization')
 <div class="top_services_filter">
     <div class="container" style="text-align:right;">
-        <input type="checkbox" class="checkbox">
+        <input type="checkbox" class="checkbox" onclick="myFunction()" id="myCheck">
         <span class="checkmark">Calendar</span>
     </div>
 </div>
-<div class="inner_services">
+<div class="inner_services" id="inner_services">
     <div id="content" class="container">
     <div class="col-sm-12 p-0 card-columns">
             @foreach($events as $event)
@@ -49,13 +54,7 @@ Events
         <div class="example col-md-12">
             <div class="row">
                 <div class="col-md-6 pagination_text">
-                <?php
-                    // EventController defines default results per page using the $pagination parameter
-
-                    // Paginate parameter defines the selected number of results in the dropdownlist on this page
-                    if (Request::get("paginate") != null) $pagination = Request::get("paginate");
-                ?>
-                    <p>Showing {{ $events->currentPage() * $pagination - intval($pagination - 1)  }}-{{ $events->currentPage() * $pagination  }} of {{ $events->total() }} items  <span>Showing {{ $pagination }} per page</span></p>
+                    <p>Showing {{ $events->currentPage() * Request::get('paginate') - intval(Request::get('paginate') - 1)  }}-{{ $events->currentPage() * Request::get('paginate')  }} of {{ $events->total() }} items  <span>Show {{ Request::get('paginate') }} per page</span></p>
                 </div>
                 <div class="col-md-6 text-right">
                     {{ $events->appends(\Request::except('page'))->render() }}
@@ -64,7 +63,116 @@ Events
         </div>
     </div>
 </div>
+<div id="wrap" onload="test_resize()">
+    <div id="calendar" style="display:none;"></div>
+</div>
+<script>
+function myFunction(){
+    var checkBox = document.getElementById("myCheck");
+    var listDisplay = document.getElementById("inner_services");
+    var calendar = document.getElementById("calendar"); 
+    if(checkBox.checked == true){
+        listDisplay.style.display = "none";
+        calendar.style.display = "block";
+    }else{
+        listDisplay.style.display = "block";
+        calendar.style.display = "none";
+    }
+}
 
+document.addEventListener('DOMContentLoaded', function() {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      headerToolbar: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth,timeGridWeek,timeGridDay'
+      },
+      initialDate: '2020-09-12',
+      navLinks: true, // can click day/week names to navigate views
+      selectable: true,
+      selectMirror: true,
+      select: function(arg) {
+        var title = prompt('Event Title:');
+        if (title) {
+          calendar.addEvent({
+            title: title,
+            start: arg.start,
+            end: arg.end,
+            allDay: arg.allDay
+          })
+        }
+        calendar.unselect()
+      },
+      eventClick: function(arg) {
+        if (confirm('Are you sure you want to delete this event?')) {
+          arg.event.remove()
+        }
+      },
+      editable: true,
+      dayMaxEvents: true, // allow "more" link when too many events
+      events: [
+        {
+          title: 'All Day Event',
+          start: '2020-09-01'
+        },
+        {
+          title: 'Long Event',
+          start: '2020-09-07',
+          end: '2020-09-10'
+        },
+        {
+          groupId: 999,
+          title: 'Repeating Event',
+          start: '2020-09-09T16:00:00'
+        },
+        {
+          groupId: 999,
+          title: 'Repeating Event',
+          start: '2020-09-16T16:00:00'
+        },
+        {
+          title: 'Conference',
+          start: '2020-09-11',
+          end: '2020-09-13'
+        },
+        {
+          title: 'Meeting',
+          start: '2020-09-12T10:30:00',
+          end: '2020-09-12T12:30:00'
+        },
+        {
+          title: 'Lunch',
+          start: '2020-09-12T12:00:00'
+        },
+        {
+          title: 'Meeting',
+          start: '2020-09-12T14:30:00'
+        },
+        {
+          title: 'Happy Hour',
+          start: '2020-09-12T17:30:00'
+        },
+        {
+          title: 'Dinner',
+          start: '2020-09-12T20:00:00'
+        },
+        {
+          title: 'Birthday Party',
+          start: '2020-09-13T07:00:00'
+        },
+        {
+          title: 'Click for Google',
+          url: 'http://google.com/',
+          start: '2020-09-28'
+        }
+      ]
+    });
+
+    calendar.render();
+  });
+
+</script>
 @endsection
 
 
